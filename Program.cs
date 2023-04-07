f70e1bb17c537c6773d757725abfd20bbf8541ec
MarceloBertochi <paidasamia@gmail.com>
2023-04-07 20:03:37 -0300

    exibição de variáveis

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace variaveisDeExibicao
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+            string nome1 = "marcelo"; // variável nome do tipo string atribuido o valor Marcelo
+            Console.WriteLine("Olá " + nome1); // o sinal de + concatena os valores
+
+            Console.WriteLine();
+
+            string nome = "Marcelo ";
+            string sobrenome = "Bertochi";
+            Console.WriteLine(nome + sobrenome);
+
+             Console.WriteLine();
+
+             int x = 10;
+             int y = 20;
+             Console.WriteLine(x + y);
+
+
+        }
+    }
+}
